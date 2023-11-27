54463855327ac440992c032480fe0965a137b92b
fadel010 <mouhamedfadel1999@gmail.com>
2023-11-27 14:23:35 +0000

    Updates

--- e4/Level4/romannumbers/main.go
+++ e4/Level4/romannumbers/main.go
@@ -1,59 +1,67 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"strconv"
+	"fmt"
 )
 
-var romanNumerals = map[int]string{
-	1:    "I",
-	5:    "V",
-	10:   "X",
-	50:   "L",
-	100:  "C",
-	500:  "D",
-	1000: "M",
-}
-
-func rn(num int) string {
-	if num <= 0 || num >= 4000 {
-		return "ERROR: cannot convert to roman digit"
-	}
-
-	result := ""
-	for num > 0 {
-		// Find the largest Roman numeral that is less than or equal to num
-		var cle int
-		var romanValue string
-		for v, n := range romanNumerals {
-			if num >= v && v > cle {
-				cle = v
-				romanValue = n
-			}
-		}
-
-		// Subtract the cle of the Roman romanValue and add it to the result
-		result += romanValue
-		num -= cle
-	}
-
-	return result
-}
-
 func main() {
-	if len(os.Args[1:]) != 1 {
+	args := os.args[1:]
+
+	if len(args) != 1 {
 		return
 	}
 
-	numStr := os.Args[1]
-	num, err := strconv.Atoi(numStr)
-	if err != nil {
-		fmt.Println("ERROR: cannot convert to roman digit")
+	input := Atoi(args[0])
+	if input <= 0 || input >= 4000 {
+		fmt.Printf("ERROR: cannot convert to roman digit.")
 		return
 	}
 
-	romanNumeral := rn(num)
+	nums := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	roman1 := []string{"M", "(M-C)", "D", "D-C", "C", "(C-X)", "L", "(L-X)", "X", "(X-I)", "V", "(V-I)", "I"}
+	roman2 := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+	roman1Result := ""
+	roman2Result := ""
 
-	fmt.Println(romanNumeral)
+	for i := 0; i < len(nums); {
+		if nums[i] <= input {
+			input -= nums[i]
+
+			roman1Result += roman1[i]
+			roman2Result += roman2[i]
+
+			if input != 0 {
+				roman1Result += "+"
+			}
+		} else {
+			i++
+		}
+	}
+	fmt.Printf(roman1Result)
+	fmt.Printf(roman2Result)
+
+}
+
+func Atoi(s string) int {
+	sign := 1
+	start := 0
+
+	if s[0] == '-' {
+		sign = -1
+		start = 1
+	} else if s[0] == '+' {
+		start = 1
+	}
+
+	result := 0
+
+	for i := start; i < len(s); i++ {
+		if s[i] >= '0' && s[i] <= '9' {
+			result = int(s[i]-'0') + result*10
+		} else {
+			return 0
+		}
+	}
+	return result * sign
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module work_hard
+module github.com/fadel010/challenge-go
 
-go 1.20
+go 1.19
 
-require github.com/01-edu/z01 v0.1.0 // indirect
+require github.com/01-edu/z01 v0.1.0
